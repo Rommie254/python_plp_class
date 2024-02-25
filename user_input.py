954158387a75f166b1c08a6d1f5961b1071fad72
Sharon Kilel <43027438+Rommie254@users.noreply.github.com>
2024-02-25 15:41:54 +0300

    Intro to Python assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+name = input ("Please enter your name: \n" )
+age = input ("Please enter your age in years: \n")
+location = input("What is your current location? \n")
+
+print ("Hello",name,", it's nice to meet you. You have entered your age as",age, "years and you currently live in ",location,".\n")
+
